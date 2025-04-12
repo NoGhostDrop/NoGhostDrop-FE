94477a82564c364bb4b7d47cbebe1c2f786a9e6f
9oodam <godamcity98@gmail.com>
2025-04-13 02:14:08 +0900

    get events

--- src/components/StatusBox/StatusBox.tsx
+++ src/components/StatusBox/StatusBox.tsx
@@ -9,9 +9,9 @@ type StatusBoxProps = {
 const StatusBox = ({ address, status, amount }: StatusBoxProps) => {
   return (
     <div className="flex items-center justify-between rounded-xl border border-white/20 bg-white/10 p-4 shadow-md backdrop-blur-md transition-transform">
-      <div>
+      <div className="flex flex-col items-start">
         <div className="text-left font-mono text-sm text-white/70">
-          {address}
+          {address ? address : "Unkown Address"}
         </div>
         <div className="text-lg font-semibold">
           Status:{" "}
@@ -31,9 +31,9 @@ const StatusBox = ({ address, status, amount }: StatusBoxProps) => {
           {status ? "Received" : "Blocked"}
         </div>
         {status ? (
-          <div className="text-xl font-bold text-[#64DCC8]">{`+${amount} $REAL`}</div>
+          <div className="text-xl font-bold text-[#64DCC8]">{`+${amount} $HUMN`}</div>
         ) : (
-          <div className="text-xl font-bold text-red-400">0 $REAL</div>
+          <div className="text-xl font-bold text-red-400">0 $HUMN</div>
         )}
       </div>
     </div>
--- src/pages/SlashList.tsx
+++ src/pages/SlashList.tsx
@@ -1,63 +1,127 @@
-import StatusBox from "@/components/StatusBox/StatusBox";
 import React, { useEffect, useRef, useState } from "react";
+import { ethers } from "ethers";
+import StatusBox from "@/components/StatusBox/StatusBox";
 
-const dummy = [
-  {
-    address: "0x42f8...d9ae",
-    status: true,
-    amount: 10,
-  },
-  {
-    address: "0x42f8...d9ae",
-    status: false,
-    amount: 0,
-  },
-  {
-    address: "0xab12...ffff",
-    status: false,
-    amount: 0,
-  },
-  {
-    address: "0xab12...ffff",
-    status: false,
-    amount: 0,
-  },
-  {
-    address: "0xab12...ffff",
-    status: false,
-    amount: 0,
-  },
-  {
-    address: "0xab12...ffff",
-    status: false,
-    amount: 0,
-  },
-  {
-    address: "0xab12...ffff",
-    status: true,
-    amount: 0,
-  },
-];
+import { abi as airdropAbi } from "../abi/Airdrop.json";
+const airdropAddress = import.meta.env.VITE_SAGA_AIRDROP_ADDRESS || "";
+
+// const dummy = [
+//   {
+//     address: "0x42f8...d9ae",
+//     status: true,
+//     amount: 10,
+//   },
+//   {
+//     address: "0x42f8...d9ae",
+//     status: false,
+//     amount: 0,
+//   },
+//   {
+//     address: "0xab12...ffff",
+//     status: false,
+//     amount: 0,
+//   },
+//   {
+//     address: "0xab12...ffff",
+//     status: false,
+//     amount: 0,
+//   },
+//   {
+//     address: "0xab12...ffff",
+//     status: false,
+//     amount: 0,
+//   },
+//   {
+//     address: "0xab12...ffff",
+//     status: false,
+//     amount: 0,
+//   },
+//   {
+//     address: "0xab12...ffff",
+//     status: true,
+//     amount: 0,
+//   },
+// ];
 
 const SlashList = () => {
   const listRef = useRef<HTMLDivElement>(null);
   const [lastStatus, setLastStatus] = useState<boolean>(false);
+  const [events, setEvents] = useState<any[]>([]);
+
+  // useEffect(() => {
+  //   if (dummy.length > 0) {
+  //     const lastItem = dummy[dummy.length - 1];
+  //     setLastStatus(lastItem.status);
+  //   }
+  // }, [dummy]);
 
   useEffect(() => {
-    if (dummy.length > 0) {
-      const lastItem = dummy[dummy.length - 1];
-      setLastStatus(lastItem.status);
-    }
-  }, [dummy]);
-
-  useEffect(() => {
+    console.log(events);
     if (listRef.current) {
       listRef.current.scrollTo({
         top: listRef.current.scrollHeight,
         behavior: "smooth",
       });
     }
-  }, [dummy]);
+  }, [events]);
+
+  useEffect(() => {
+    const fetchPastEvents = async () => {
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const contract = new ethers.Contract(
+        airdropAddress,
+        airdropAbi,
+        provider,
+      );
+
+      // 과거 이벤트 가져오기
+      const filter = contract.filters.Claimed();
+      const logs = await provider.getLogs({
+        ...filter,
+        fromBlock: 0, // 시작 블록 번호
+        toBlock: "latest", // 최신 블록까지
+      });
+
+      const pastEvents = logs
+        .map((log) => contract.interface.parseLog(log))
+        .filter((parsedLog) => parsedLog && parsedLog.args.token)
+        .map((parsedLog) => {
+          console.log(parsedLog);
+          if (parsedLog) {
+            return {
+              token: parsedLog.args.token,
+              to: parsedLog.args.user,
+              amount: ethers.formatUnits(parsedLog.args.amount, 18),
+            };
+          }
+        });
+
+      setEvents(pastEvents);
+    };
+
+    fetchPastEvents();
+
+    // 실시간 이벤트 구독
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const contract = new ethers.Contract(airdropAddress, airdropAbi, provider);
+
+    const handleClaimedEvent = (
+      token: string,
+      to: string,
+      amount: ethers.BigNumberish,
+    ) => {
+      setEvents((prevEvents) => [
+        ...prevEvents,
+        { token, to, amount: amount.toString() },
+      ]);
+    };
+
+    contract.on("Claimed", handleClaimedEvent);
+
+    return () => {
+      contract.off("Claimed", handleClaimedEvent);
+    };
+  }, []);
 
   return (
     <div className="box-border flex h-[90vh] flex-col items-center justify-center bg-gradient-to-b from-[#1c1c1c] to-[#3a2f2f] px-6 py-12 text-white">
@@ -69,15 +133,18 @@ const SlashList = () => {
         ref={listRef}
         className="w-full max-w-4xl flex-1 space-y-4 overflow-auto"
       >
-        {dummy.map((status) => {
-          return (
-            <StatusBox
-              address={status.address}
-              status={status.status}
-              amount={status.amount}
-            />
-          );
-        })}
+        {events.length > 0 &&
+          events.map((status, index) => {
+            console.log(status);
+            return (
+              <StatusBox
+                key={index}
+                address={status.to}
+                status={true}
+                amount={status.amount}
+              />
+            );
+          })}
       </div>
     </div>
   );
